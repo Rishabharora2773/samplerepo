7544506cfe0fe76d13cf1611af1973448c3786e8
Rishabharora2773 <rishabharora2773@gmail.com>
2019-07-29 13:13:26 +0530

    Initial Commit

---
+++ Calc.java
@@ -0,0 +1,21 @@
+public class Calc {
+    public static int add(int x, int y) {
+        return x + y;
+    }
+    
+    public static int sub(int x, int y) {
+        return x - y;
+    }
+    
+    public static int mult(int x, int y) {
+        return x * y;
+    }
+    
+    public static int div(int x, int y) {
+        return x / y;
+    }
+    
+	public static void main (String[] args) {
+		System.out.println(add(1,3));
+	}
+}
